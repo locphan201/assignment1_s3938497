41e9bf6611ec6e1b010820a6e781d60d64493858
Loc Phan <s3938497@rmit.edu.vn>
2023-08-02 10:54:51 +0700

    Initial Commit

---
+++ assignment1_s3938497/assignment1_s3938497App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1_s3938497App.swift
+//  assignment1_s3938497
+//
+//  Created by Loc Phan Vinh on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct assignment1_s3938497App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
