fd133d7a84259ffdaa127f5c002471eac0e21713
Loc Phan <s3938497@rmit.edu.vn>
2023-08-07 14:09:38 +0700

    Comment

--- assignment1_s3938497/Model/ModelData.swift
+++ assignment1_s3938497/Model/ModelData.swift
@@ -1,9 +1,14 @@
-//
-//  ModelData.swift
-//  assignment1_s3938497
-//
-//  Created by Loc Phan Vinh on 02/08/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 1
+  Author: Phan Vinh Loc
+  ID: 3938497
+  Created  date: 02/08/2023
+  Last modified: 07/08/2023
+  Acknowledgement: N/A
+*/
 
 import Foundation
 import CoreLocation
